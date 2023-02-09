bd5b13bed790d8c28838b0fdb2466e092189cce2
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2023-02-09 10:44:38 +0100

    style: Change log output format

--- src/utils/custom_logger.py
+++ src/utils/custom_logger.py
@@ -1,7 +1,7 @@
 import logging
 
 # Set up default logger for project
-formatter = logging.Formatter("%(levelname)s: %(message)s")
+formatter = logging.Formatter("[%(levelname)s]\t %(message)s")
 log = logging.getLogger("mysql-util")
 ch = logging.StreamHandler()
 ch.setFormatter(formatter)
