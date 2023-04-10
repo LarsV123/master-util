4644e2b674949cf4222734bd830a282bd27ad495
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2023-04-10 09:02:41 +0200

    feat: Add more benchmarking

--- src/benchmarks.py
+++ src/benchmarks.py
@@ -2,11 +2,23 @@ import locale
 from db import Connector
 from utils.custom_logger import log
 from utils.profile import get_runtime
+from utils import experiment_logger
 from tabulate import tabulate
 from tqdm import tqdm
 
 # This is used to format numbers with thousands separators (e.g. 123 456 780).
 locale.setlocale(locale.LC_ALL, "")
+
+# Pairs of collations which should be equivalent
+COLLATIONS = [
+    ("utf8mb4_nb_icu_ai_ci", "utf8mb4_nb_0900_ai_ci"),
+    ("utf8mb4_us_icu_ai_ci", "utf8mb4_0900_ai_ci"),
+    ("utf8mb4_us_icu_as_cs", "utf8mb4_0900_as_cs"),
+    ("utf8mb4_fr_icu_ai_ci", "utf8mb4_0900_ai_ci"),
+    ("utf8mb4_zh_icu_0900_as_cs", "utf8mb4_zh_0900_as_cs"),
+    ("utf8mb4_ja_icu_0900_as_cs", "utf8mb4_ja_0900_as_cs"),
+    ("utf8mb4_ja_icu_0900_as_cs_ks", "utf8mb4_ja_0900_as_cs_ks"),
+]
 
 
 def performance_benchmark():
@@ -16,40 +28,65 @@ def performance_benchmark():
 
     loops = 3
     table = "test1_no_NO"
-    collations = [
-        "utf8mb4_nb_icu_ai_ci",
-        "utf8mb4_nb_0900_ai_ci",
-        "utf8mb4_us_icu_ai_ci",
-        "utf8mb4_0900_ai_ci",
-    ]
 
-    results = []
-    pbar = tqdm(collations)
+    # Find all collations to test
+    collations = []
+    for c1, c2 in COLLATIONS:
+        collations.append(c1)
+        collations.append(c2)
+    collations = list(set(collations))  # Remove duplicates, keep order
 
-    for collation in pbar:
-        tqdm.write(f"Testing collation: {collation}")
-        result = {"collation": collation}
+    steps = len(COLLATIONS) * 3 * 2
+    pbar = tqdm(steps)
 
-        pbar.set_description("order_by_asc")
-        result["order_by_asc"] = benchmark_order_by(
-            conn, table, collation, repetitions=loops, ascending=True
-        )
+    experiment_logger.init()
 
-        pbar.set_description("order_by_desc")
-        result["order_by_desc"] = benchmark_order_by(
-            conn, table, collation, repetitions=loops, ascending=False
-        )
-
-        pbar.set_description("equals")
-        result["equals"] = benchmark_equals(conn, table, collation, repetitions=loops)
-
-        tqdm.write(f"Done testing {collation}")
-        results.append(result)
+    for _ in range(loops):
+        for collation in collations:
+            test_collation(conn, table, collation, pbar)
 
     pbar.close()
     conn.close()
     print("*" * 80)
-    print(tabulate(results, headers="keys", tablefmt="mysql"))
+    results = experiment_logger.get_results()
+    print(
+        tabulate(
+            results,
+            headers=[
+                "Collation",
+                "Table",
+                "Avg. order_by_asc",
+                "Avg. order_by_desc",
+                "Avg. equals",
+                "Iterations",
+            ],
+            tablefmt="mysql",
+        )
+    )
+
+
+def test_collation(conn: Connector, table: str, collation: str, pbar: tqdm):
+    """Run all performance benchmarks for a given collation."""
+    log.debug(f"Testing collation: {collation}")
+    tqdm.write(f"Testing collation: {collation}")
+    result = {"collation": collation, "data_table": table}
+
+    pbar.set_description("order_by_asc")
+    result["order_by_asc"] = benchmark_order_by(conn, table, collation, ascending=True)
+    pbar.update(1)
+
+    pbar.set_description("order_by_desc")
+    result["order_by_desc"] = benchmark_order_by(
+        conn, table, collation, ascending=False
+    )
+    pbar.update(1)
+
+    pbar.set_description("equals")
+    result["equals"] = benchmark_equals(conn, table, collation)
+    pbar.update(1)
+
+    tqdm.write(f"Done testing {collation}")
+    experiment_logger.log_benchmark(result)
 
 
 def validity_tests():
@@ -57,7 +94,6 @@ def validity_tests():
     conn = Connector()
     pre_check(conn)
 
-    # table = "country_list_en_US"
     table = "country_list_no_NO"
     collations = [
         ("utf8mb4_nb_icu_ai_ci", "utf8mb4_nb_0900_ai_ci"),
@@ -120,9 +156,7 @@ def pre_check(db: Connector):
     log.info(f"Table test1_no_NO has {count:n} rows. {min_value=}, {max_value=}")
 
 
-def benchmark_order_by(
-    db: Connector, table: str, collation: str, repetitions: int, ascending: bool
-):
+def benchmark_order_by(db: Connector, table: str, collation: str, ascending: bool):
     direction = "ASC" if ascending else "DESC"
     query = f"""
     -- sql
@@ -131,31 +165,28 @@ def benchmark_order_by(
     {direction} LIMIT 1;
     """
     log.debug(f"{query=}")
-    log.debug(f"{repetitions=} | {ascending=}")
+    log.debug(f"{ascending=}")
 
     @get_runtime
     def timed_order_by():
-        for _ in range(repetitions):
-            db.cursor.execute(query)
-            result = db.cursor.fetchall()
-        return result
+        db.cursor.execute(query)
 
-    return timed_order_by()
+    runtime = timed_order_by()
+    db.cursor.fetchall()
+    return runtime
 
 
-def benchmark_equals(db: Connector, table: str, collation: str, repetitions=2):
+def benchmark_equals(db: Connector, table: str, collation: str):
     query = f"""
     SELECT * FROM my_project.{table}
     WHERE value = 'Norge123' COLLATE {collation};
     """
     log.debug(f"{query=}")
-    log.debug(f"{repetitions=}")
 
     @get_runtime
     def timed_equals():
-        for _ in range(repetitions):
-            db.cursor.execute(query)
-            result = db.cursor.fetchall()
-        return result
+        db.cursor.execute(query)
 
-    return timed_equals()
+    runtime = timed_equals()
+    db.cursor.fetchall()
+    return runtime
