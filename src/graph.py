312e55a3529b440c81c5efc7e18314c8a2b2fe35
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2023-05-29 21:56:00 +0200

    feat: Improve bar plot

--- src/graph.py
+++ src/graph.py
@@ -26,25 +26,44 @@ def identify_icu_config(row):
 # Connect to the database
 conn = sqlite3.connect("../final_results.db")
 
-
 # Fetch the data from the SQLite database
 query = "SELECT * FROM benchmarks"
 df = pd.read_sql_query(query, conn)
 
+# Filter the data to include only those with 'data_size' equals to 1000000.
+df = df[df["data_size"] == 1000000]
+
 # Identify the ICU configuration and add it to a new column
 df["ICU_config"] = df.apply(identify_icu_config, axis=1)
 
-# Calculate the median over the 10 runs for each configuration
-df_median = (
-    df.groupby(["locale", "ICU_config", "collation", "data_size"])[
-        ["order_by_asc", "order_by_desc", "equals"]
-    ]
-    .median()
+"""
+Calculate median of 'order_by_asc', 'order_by_desc' and 'equals' for each
+configuration, as well as the standard deviation.
+"""
+df_stats = (
+    df.groupby(["locale", "ICU_config", "collation", "data_size"])
+    .agg(
+        {
+            "order_by_asc": ["median", "std"],
+            "order_by_desc": ["median", "std"],
+            "equals": ["median", "std"],
+        }
+    )
     .reset_index()
 )
 
-# Filter the data to include only those with 'data_size' equals to 1000000.
-df_median = df_median[df_median["data_size"] == 1000000]
+
+# Join all multi-level columns (i.e. ["order_by_asc", "median"] etc)
+df_stats.columns = [
+    f"{x[0]}_{x[1]}" if x[1] else x[0] for x in df_stats.columns.ravel()
+]
+
+
+# Debugging only: show all data in the dataframe
+# pd.set_option("display.max_columns", None)
+# print(df_stats.describe())
+# print(df_stats.info())
+# print(df_stats.sample(10))
 
 # Set destination folder for plots
 plot_dir = os.path.join("..", "plots", "experiment1")
@@ -52,24 +71,32 @@ plot_dir = os.path.join("..", "plots", "experiment1")
 # Create the directory if it doesn't exist
 os.makedirs(plot_dir, exist_ok=True)
 
-for group in COLLATIONS[:1]:
-    # Filter the data for the current group
-    df_group = df_median[
-        (
-            (df_median["collation"] == group["icu"])
-            | (df_median["collation"] == group["mysql"])
-        )
-        & (df_median["locale"] == group["locale"])
-    ]
+for metric in ["order_by_asc", "order_by_desc", "equals"]:
+    for group in COLLATIONS[:2]:
+        df_group = df_stats[
+            (df_stats["collation"].isin([group["icu"], group["mysql"]]))
+            & (df_stats["locale"] == group["locale"])
+            & (df_stats["data_size"] == 1000000)
+        ]
 
-    df_median = df_group.groupby(["ICU_config"])[
-        ["order_by_asc", "order_by_desc", "equals"]
-    ]
+        # Debugging only: show all data in the dataframe
+        # pd.set_option("display.max_columns", None)
+        # print(df_group.describe())
+        # print(df_group.info())
+        # print(df_group.sample(4))
 
-    # For each metric, create a bar plot.
-    for metric in ["order_by_asc", "order_by_desc", "equals"]:
+        # Create plot with error bars
         plt.figure(figsize=(10, 6))
-        sns.barplot(data=df_group, x="ICU_config", y=metric)
+        median_column = f"{metric}_median"
+        std_column = f"{metric}_std"
+        sns.barplot(
+            data=df_group,
+            x="ICU_config",
+            y=(median_column),
+            yerr=df_group[std_column],
+        )
+
+        # Add title and label
         plt.title(f"Median execution time for operation '{metric}'")
         plt.ylabel("Time (s)")
 
